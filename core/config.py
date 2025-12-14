3f41d71451c75e1a9d6c7dcf3ee6bbd8f4e44048
MeetSanghvi2003 <meetsanghvi2347@gmail.com>
2025-12-14 13:54:48 +0530

    first commit

--- core/config.py
+++
@@ -1,9 +0,0 @@
-from os import getenv
-from dotenv import load_dotenv
-
-load_dotenv()
-
-
-class Config:
-    convex_url: str = getenv("CONVEX_URL")
-    b_a = ["jh70ns6tjkyrj3n0va5s292c017vdq98","jh78y4mnxyxsmhj2tbwcwqxa497vdb5p","jh77v9c3j5npptckcm5cgrc6zx7vc376","jh7dkhq6xrccfe19rmwb5h0y657vdq67","jh7951gzjejh0mxr1frcxkwz157vdccd","jh7e68q8t6ghhr6mac0x29h0617vdbc0"]
--- utils/returnFormat.py
+++
@@ -1,5 +0,0 @@
-from typing import Literal
-
-
-def returnFormat(type_: Literal["success", "error"], message: str, data=None) -> dict:
-    return {"type": type_, "data": data, "message": message}
